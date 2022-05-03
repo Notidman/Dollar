8db3802581d5dc4787daf73449c8d301a0d666e8
HollowKaeden <Vanya200373@yandex.ru>
2022-05-03 20:29:20 +0300

    --comment json struct

--- jsonparser.cpp
+++ jsonparser.cpp
@@ -2,5 +2,50 @@
 
 JsonParser::JsonParser()
 {
-
 }
+
+/*
+{
+  "name": "Dollar",
+  "language": "C++",
+  "tags": "C++;Qt;OpenCV",
+  "control_system": "Git",
+  "build_system": "CMake",
+  "libraries":
+    [
+    "Qt",
+    "SFML"
+    ]
+  "struct":
+    [
+      {
+        "name": "Hello",
+        "type": "d",
+        "children": []
+      },
+      {
+        "name": "Hello2",
+        "type": "d",
+        "children":
+          [
+            {
+              "name": "Arividerci",
+              "type": "d",
+              "children":
+                [
+                  {
+                    "name": "Hi.cpp",
+                    "type": "f"
+                  }
+                ]
+            }
+          ]
+      },
+      {
+        "name": "Adios",
+        "type": "d",
+        "children": []
+      }
+    ]
+}
+*/
